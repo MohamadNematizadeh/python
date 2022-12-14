dd51a679177dc400c5aa7c13f5110164b2353287
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2022-12-14 17:05:26 +0330

    Update store.py

--- Assignment 7/store.py
+++ Assignment 7/store.py
@@ -1,25 +1,25 @@
+import pyfiglet
+def Title():
+    title = pyfiglet.figlet_format("Mohamad Store@-@",font="slant")
+    print(title)
 PRODUCTS = []
-
 def read_from_databse():
-    f=open("databast.text","r")
-    for line in f:
+     f=open("databast.text","r")
+     for line in f:
         result = line.split(",")
-        my_dict = {"code":result[0],"name":result [2], "count":result[3]}
+        my_dict = {"code":result[0],"name":result [2], "count":result[3],"price": result[2]}
         PRODUCTS.append(my_dict)
-    f.close()
-
-
-    
-
+     f.close()
+# .-.-.-.-.-.-.-.-.-.-.-.
 def show_menu():
-    print("1-Add")    
+    print("1-Add") 
     print("2-Edit")    
     print("3-Removoe")    
     print("4-Search")
     print("5-Show List")
     print("6-Buy")
     print("7-Exit")    
-
+# .-.-.-.-.-.-.-.-.-.-.-.
 def add():
     code = input("enter code:")
     name = input("enter name:")
@@ -27,46 +27,72 @@ def add():
     count = input("enter count:")
     new = {'code':code, 'name':name,'price':price,'count':count }
     PRODUCTS.append(new)
-
+# .-.-.-.-.-.-.-.-.-.-.-.
 def Edit():
-  while True:
-        edit_from_name = input("Enter name product:\n")
+        id = int(input("enter your code:"))
         for i in range(len(PRODUCTS)):
-            if edit_from_name == PRODUCTS[i]["name"]:
-                edit_item = int(input('1.Edit id\n 2.Edit name\n 3.Edit price\n 4.Edit count\nEnter number:'))
-                if edit_item == 1 :
-                    PRODUCTS[i]["id"] = input("Enter new id:\n")
-                elif edit_item == 2 :
-                    PRODUCTS[i]["name"] = input("Enter new name:\n")
-                elif edit_item == 3 :
-                    PRODUCTS[i]["price"] = input("Enter new pice:\n")
-                elif edit_item == 4 :
-                    PRODUCTS[i]["count"] = input("Enter new count:\n")
-        edit_repeat = input('do you edit enother product(Y/N):')
-        if edit_repeat == 'n' or 'N':
-            break
+            if PRODUCTS[i]['code']==id:
+             while True:
+                  print("1-Edit code")    
+                  print("2-Edit name")    
+                  print("3-Edit price")  
+                  edit=int(input("Pls Choose from Edit Menu : "))
+                  if edit == 1:
+                    name=input("Pls Enter New Name : ")
+                    PRODUCTS[i]['Name']=name
+                    print("Product Edited Successfully!")
+                  elif edit==2:
+                    price=float(input("Pls Enter New Price : "))
+                    PRODUCTS[i]['Price']=price
+                    print("Product Edited Successfully!")
+                  elif edit==3:
+                    count=int(input("Pls Enter New Count : "))
+                    PRODUCTS[i]['Count']=count
+                    print("Product Edited Successfully!")
+                  elif edit==4:
+                    break
+                  else:
+                    print("Value Error ,Try Again !")
+# .-.-.-.-.-.-.-.-.-.-.-.
 def Removoe():
-    pass
-
+        A = 0
+        code = input("enter code: ")
+        for product in PRODUCTS:
+            code_a = A + 1
+            if product["code"] == code:
+                del PRODUCTS[A-1]
+                print("Your item has been successfully deleted")
+                break
+        else:
+            print("not found")
+# .-.-.-.-.-.-.-.-.-.-.-.
 def Search():
     user = input("type your:")
     for product in PRODUCTS:
         if product["code"] == user or product["name"]==user:
-            print(product["code"],"/t",product["name"],"",product["price"])
-            break
+             print(product["code"],"\t",product["name"],"\t",product["count"])
+             break
     else:
         print("not found")        
-
+# .-.-.-.-.-.-.-.-.-.-.-.
 def Show_list():
-
-    print("code/neme/price")
+     print("cod\tname\tcount")
+     for product in PRODUCTS:
+        print(product["code"],"\t",product["name"],"\t",product["count"])
+# .-.-.-.-.-.-.-.-.-.-.-.
+def QR_code():
+    code_qr = input("code: ")
     for product in PRODUCTS:
-         print(product["code"],"/t",product["name"],"",product["price"])
-
+        if product["code"] == code_qr:
+            img = qrcode.make(product)
+            img.save("product_qr.png")
+            break
+    else:
+        print("not found")
+# .-.-.-.-.-.-.-.-.-.-.-.
 def Buy():
     ...
-
-
+Title()
 print("Wellcom to Mohammad Store")
 print("Loading...")
 read_from_databse()
@@ -78,18 +104,22 @@ while True:
 
     if choice == 1 :
         add()
+
     elif choice == 2 :
         Edit()
+
     elif choice == 3 :
         Removoe()
+
     elif choice == 4 :
         Search()
+
     elif choice == 5 :
         Show_list()  
     elif choice == 6 :
         Buy()
     elif choice == 7 :
-        Edit()     
+        Exit(0)     
 
 # ._._._._._._._._._._._._._._.
 # for product in PRODUCTS:  
